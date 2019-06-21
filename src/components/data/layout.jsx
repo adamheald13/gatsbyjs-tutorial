80f562f80d7e9b94b0df3f7f61b9f20b6755ceb1
Adam Heald <adamheald13@gmail.com>
2019-06-20 20:49:35 -0500

    Wrap data layout in Container

--- src/components/data/layout.jsx
+++ src/components/data/layout.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link, useStaticQuery, graphql } from 'gatsby'
+import Container from '../shared/container'
 import { css } from '@emotion/core'
 
 import { rhythm } from '../../utils/typography'
@@ -18,34 +19,36 @@ export default ({ children }) => {
   )
 
   return (
-    <div
-      css={css`
-        margin: 0 auto;
-        max-width: 700px;
-        padding ${rhythm(2)};
-        padding-top: ${rhythm(1.5)};
-      `}
-    >
-      <Link to ={'/data'}>
-        <h3
-          css={css`
-            margin-bottom: ${rhythm(2)};
-            display: inline-block;
-            font-style: normal;
-          `}
-        >
-          {data.site.siteMetadata.title}
-        </h3>
-      </Link>
-      <Link
-        to={'/data/about'}
+    <Container>
+      <div
         css={css`
-          float: right;
+          margin: 0 auto;
+          max-width: 700px;
+          padding ${rhythm(2)};
+          padding-top: ${rhythm(1.5)};
         `}
       >
-        About
-      </Link>
-      {children}
-    </div>
+        <Link to ={'/data'}>
+          <h3
+            css={css`
+              margin-bottom: ${rhythm(2)};
+              display: inline-block;
+              font-style: normal;
+            `}
+          >
+            {data.site.siteMetadata.title}
+          </h3>
+        </Link>
+        <Link
+          to={'/data/about'}
+          css={css`
+            float: right;
+          `}
+        >
+          About
+        </Link>
+        {children}
+      </div>
+    </Container>
   )
 } 
